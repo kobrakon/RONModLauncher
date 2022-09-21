a7b19f6784be3828e54be68905903127e5c8239a
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-09-21 19:51:59 -0400

    update for config and other shit idk i forgor

--- RON Mod Launcher/script.cs
+++ RON Mod Launcher/script.cs
@@ -1,28 +1,34 @@
+using System.Xml;
+using System.Xml.Linq;
 using System.Diagnostics;
 using System.Reflection;
 
-string gamePath;
-string paks;
-string gamePaks;
-string vo;
-string banks;
-string me;
-string importantfileithinkpath;
-string modvo;
-string modbanks;
-string gamevotemp;
-string gamebanktemp;
-IEnumerable<string> gamevoli;
-IEnumerable<string> mevoli;
-IEnumerable<string> gamebankli;
-IEnumerable<string> mebankli;
-Process gameProcess;
+string gamePath = string.Empty;
+string configpath = string.Empty;
+string paks = string.Empty;
+string gamePaks = string.Empty;
+string vo = string.Empty;
+string banks = string.Empty;
+string me = string.Empty;
+string importantfileithinkpath = string.Empty;
+string modvo = string.Empty;
+string modbanks = string.Empty;
+string gamevotemp = string.Empty;
+string gamebanktemp = string.Empty;
+List<string> gamevoli = new();
+List<string> mevoli = new();
+List<string> gamebankli = new();
+List<string> mebankli = new();
+Process gameProcess = new();
+bool IsOverride = false;
+
 Main();
 
 void Main()
 {
     me = Assembly.GetExecutingAssembly().Location.TrimEnd(@"RON Mod Launcher.dll".ToCharArray());
-    importantfileithinkpath = me + @"\importantfile.txt";
+    importantfileithinkpath = $@"{me}\importantfile.txt";
+    configpath = $@"{me.Replace("\n", "").Replace("\r", "")}\config.xml";
     Console.WriteLine("RON Mod Launcher Started");
     Console.WriteLine(Environment.NewLine);
 
@@ -30,10 +36,10 @@ void Main()
     {
         Console.WriteLine("Data file not found! Making another...");
         HandleFirstTime();
-        HandleIOShit();
+        HandleIO();
         Console.ReadLine();
     } else
-    HandleIOShit();
+    HandleIO();
     Console.ReadLine(); // seriously who decided that the console should just auto kill itself when Main() is done regardless of if other stuff is running?
     // cant async it either
 }
@@ -42,15 +48,16 @@ void StartDaGame()
 {
     Console.WriteLine("Starting Game...");
     if (!File.Exists($@"{gamePath}\ReadyOrNot.exe".Replace("\n", "").Replace("\r", ""))) { Console.WriteLine("EXE not found! Probable invalid file pointer; seeking..."); HandleFirstTime(); return; } else
+    Console.WriteLine("Start in DX12 Y/N");
+    ConsoleKey response = Console.ReadKey(false).Key;
+    while (response != ConsoleKey.Y && response != ConsoleKey.N) { response = Console.ReadKey(false).Key; }
     try
     {
-        Console.WriteLine("Start in DX12 Y/N");
-        ConsoleKey response = Console.ReadKey(false).Key;
-        while (response != ConsoleKey.Y && response != ConsoleKey.N) { response = Console.ReadKey(false).Key; }
         var proc = new ProcessStartInfo()
         {
-            FileName = $@"{gamePath}\ReadyOrNot.exe".Replace("\n", "").Replace("\r", ""),
-            Arguments = response == ConsoleKey.Y ? "dx12" : "dx11"
+            FileName = $@"{gamePath}\ReadyOrNot.exe",
+            Arguments = response == ConsoleKey.Y ? "dx12" : "dx11",
+            UseShellExecute = true
         };
         gameProcess = Process.Start(proc);
         CleanupAsync();
@@ -58,7 +65,7 @@ void StartDaGame()
     }
     catch (Exception e)
     {
-        ThrowException(e, $@"{gamePath}\ReadyOrNot.exe".Replace("\n", "").Replace("\r", ""), "StartDaGame() => Process.Start()");
+        ThrowException(e, $@"{gamePath}\ReadyOrNot.exe", "StartDaGame() => Process.Start()", "An attempt to start a new game process resulted in an exception; Ensure that the game is installed.");
         Console.ReadLine();
         return;
     }
@@ -80,22 +87,53 @@ async void CleanupAsync()
 
     if (!timepassed) Console.WriteLine("steam has to be started first dumbass");
     Console.WriteLine("Game exited, cleaning up...");
-    gamevoli = Directory.GetDirectories(gamevotemp);
-    mevoli = Directory.GetDirectories(vo);
-    
-    gamevoli.ToList().ForEach((string f) =>
+    gamevoli = Directory.GetDirectories(gamevotemp).ToList();
+    mevoli = Directory.GetDirectories(vo).ToList();
+
+    if (!IsOverride)
     {
-        Directory.EnumerateFiles(f).ToList().ForEach((string b) =>
+        mevoli.ForEach((string f) =>
         {
-            string[] filei = b.Split(@"\");
-            string filef = filei[^2];
-            string fileo = filei.Last();
-            Directory.Move($@"{vo}\{filef}\{fileo}", $@"{modvo}\{filef}\{fileo}");
-            Directory.Move(b, $@"{vo}\{filef}\{fileo}");
-            Console.WriteLine($@"I/O => Returned VO {filef}\{fileo}");
+            Directory.EnumerateFiles(f).ToList().ForEach((string b) =>
+            {
+                string[] filei = b.Split(@"\");
+                string direc = filei[^2];
+                string file = filei.Last();
+                if (File.Exists($@"{modvo}\{direc}\{file}")) Directory.Move(b, $@"{modvo}\{direc}\{file}");
+                if (File.Exists($@"{gamebanktemp}\{direc}\{file}")) Directory.Move($@"{gamebanktemp}\{direc}\{file}", $@"{vo}\{direc}\{file}");
+                Console.WriteLine($@"I/O => Returned VO {direc}\{file}");
+            });
         });
-        Directory.Delete(f);
-    });
+        gamevoli.ForEach((string f) => Directory.Delete(f));
+    } else
+    {
+        mevoli.ForEach((string f) =>
+        {
+            Directory.EnumerateFiles(f).ToList().ForEach((string b) =>
+            {
+                string[] filei = b.Split(@"\");
+                string direc = filei[^2];
+                string file = filei.Last();
+                if (Directory.Exists($@"{gamevotemp}\{direc}"))
+                {
+                    Directory.Move(b, $@"{modvo}\{direc}\{file}");
+                    Console.WriteLine($@"I/O => Returned VO {direc}\{file}");
+                }
+            });
+        });
+        gamevoli.ForEach((string f) =>
+        {
+            Directory.EnumerateFiles(f).ToList().ForEach((string b) =>
+            {
+                string[] filei = b.Split(@"\");
+                string direc = filei[^2];
+                string file = filei.Last();
+                Directory.Move(b, $@"{vo}\{direc}\{file}");
+            });
+            Directory.Delete(f);
+        });
+    }
+
 
     Directory.EnumerateFiles(gamebanktemp).ToList().ForEach((string s) =>
     {
@@ -104,10 +142,10 @@ async void CleanupAsync()
         Directory.Move(s, $@"{banks}\{f}");
         Console.WriteLine($"I/O => Returned FMOD bank {f}");
     });
-
+    
     Directory.EnumerateFiles(gamePaks).ToList().ForEach((string s) =>
     {
-        if (s.Contains("pakchunk99"))
+        if (s.Contains("pakchunk99") || s.Contains("pakchunk-99"))
         {
             Directory.Move(s, $@"{paks}\{s.Split(@"\").Last()}");
             Console.WriteLine($@"I/O => Returned PAK {s.Split(@"\").Last()}");
@@ -119,46 +157,113 @@ async void CleanupAsync()
     Environment.Exit(0);
 }
 
-void HandleIOShit()
+void HandleIO()
 {
     var file = File.ReadAllText(importantfileithinkpath);
-
     DeclarePaths(file);
-    try { gamevoli = Directory.GetDirectories(vo); } catch (Exception e) { ThrowException(e, vo, "HandleIOShit() => Directory.GetDirectories()"); return; }
-    try { gamebankli = Directory.GetFiles(banks); } catch (Exception e) { ThrowException(e, banks, "HandleIOShit() => Directory.GetFiles()"); return; }
-    try { mevoli = Directory.GetDirectories(modvo); } catch (Exception e) { ThrowException(e, modvo, "HandleIOShit() => Directory.GetDirectories()"); return; }
-    try { mebankli = Directory.GetFiles(modbanks); } catch (Exception e) { ThrowException(e, modbanks, "HandleIOShit() => Directory.GetFiles()"); return; }
-    Console.WriteLine("I/O => Moving files...");
-
-    mevoli.ToList().ForEach((string f) =>
-    {
-        Directory.EnumerateFiles(f).ToList().ForEach((string b) => 
-        {
-            string[] filei = b.Split(@"\");
-            string filef = filei[^2];
-            string fileo = filei.Last();
-            Directory.CreateDirectory($@"{gamevotemp}\{filef}");
-            Directory.Move($@"{vo}\{filef}\{fileo}", $@"{gamevotemp}\{filef}\{fileo}");
-            Directory.Move(b, $@"{vo}\{filef}\{fileo}");
-            Console.WriteLine($@"I/O => Moved {filef}\{fileo} from mod VO to game VO");
-        });
-    });
-
-    mebankli.ToList().ForEach((string f) => 
-    {
-        string filei = f.Split(@"\").Last();
-
-        Directory.Move($@"{banks}\{filei}", $@"{gamebanktemp}\{filei}");
-        Directory.Move(f, $@"{banks}\{filei}");
-        Console.WriteLine($"I/O => Moved {filei} from mod banks to game FMOD banks");
-    });
-
-    Directory.EnumerateFiles(paks).ToList().ForEach((string f) => 
+    try 
     { 
-        Directory.Move(f, $@"{gamePaks}\{f.Split(@"\").Last()}"); 
-        Console.WriteLine($@"I/O => Moved PAK {f.Split(@"").Last()}"); 
-    });
+        gamevoli = Directory.GetDirectories(vo).ToList();
+        gamebankli = Directory.GetFiles(banks).ToList();
+        mevoli = Directory.GetDirectories(modvo).ToList();
+        mebankli = Directory.GetFiles(modbanks).ToList();
+        Console.WriteLine("I/O => Moving files...");
+    } catch (Exception e)
+    {
+        ThrowException(e, $@"{gamePath}", "HandleIO() => Directory.GetDirectories/GetFiles()", "I/O attempted to read game/mod files and encountered an exception");
+    }
 
+    bool[] configvals = GetConfigValues();
+
+    switch (configvals[1])
+    {
+        case true:
+            Console.WriteLine("I/O => Skip VO is set to true, VO loading skipped!");
+        break;
+        case false:
+        if (configvals[0])
+        {
+            IsOverride = true;
+            Console.WriteLine("I/O => Override all is set to true, all VO files will be moved");
+            Console.WriteLine("This may result in silent lines caused by missing files");
+            gamevoli.ForEach((string f) => 
+            {
+                Directory.EnumerateFiles(f).ToList().ForEach((string b) =>
+                {
+                    string[] e = b.Split(@"\");
+                    string direc = e[^2];
+                    string file = e.Last();
+                    if (Directory.Exists($@"{modvo}\{direc}"))
+                    {
+                        Directory.CreateDirectory($@"{gamevotemp}\{direc}");
+
+                        Directory.Move(b, $@"{gamevotemp}\{direc}\{file}");
+                    }
+                });
+            });
+            mevoli.ForEach((string f) => 
+            {
+                Directory.EnumerateFiles(f).ToList().ForEach((string b) =>
+                {
+                    string[] e = b.Split(@"\");
+                    string direc = e[^2];
+                    string file = e.Last();
+                    if (Directory.Exists($@"{vo}\{direc}"))
+                    {
+                        Directory.Move($@"{modvo}\{direc}\{file}", $@"{vo}\{direc}\{file}");
+                        Console.WriteLine($@"I/O => Moved {direc}\{file} from mod VO to game VO");
+                    }
+                });
+            });
+            break;
+        }
+        mevoli.ForEach((string f) =>
+        {
+            Directory.EnumerateFiles(f).ToList().ForEach((string b) => 
+            {
+                string[] filei = b.Split(@"\");
+                string filef = filei[^2];
+                string fileo = filei.Last();
+                Directory.CreateDirectory($@"{gamevotemp}\{filef}");
+                Directory.Move($@"{vo}\{filef}\{fileo}", $@"{gamevotemp}\{filef}\{fileo}");
+                Directory.Move(b, $@"{vo}\{filef}\{fileo}");
+                Console.WriteLine($@"I/O => Moved {filef}\{fileo} from mod VO to game VO");
+            });
+        });
+        break;
+    }
+
+    switch (configvals[2])
+    {
+        case true:
+            Console.WriteLine("I/O => Skip FMOD is set to true, FMOD loading skipped!");
+        break;
+        case false:
+            mebankli.ForEach((string f) => 
+            {
+                string filei = f.Split(@"\").Last();
+
+                Directory.Move($@"{banks}\{filei}", $@"{gamebanktemp}\{filei}");
+                Directory.Move(f, $@"{banks}\{filei}");
+                Console.WriteLine($"I/O => Moved {filei} from mod banks to game FMOD banks");
+            });
+        break;
+    }
+
+    switch (configvals[3])
+    {
+        case true:
+            Console.WriteLine("I/O => Skip PAK is set to true, PAK loading skipped!");
+        break;
+        case false:
+            Directory.EnumerateFiles(paks).ToList().ForEach((string f) => 
+            { 
+                Directory.Move(f, $@"{gamePaks}\{f.Split(@"\").Last()}"); 
+                Console.WriteLine($@"I/O => Moved PAK {f.Split(@"").Last()}"); 
+            });
+        break;
+    }
+    
     Console.WriteLine(Environment.NewLine);
     Console.WriteLine("Files have been moved, if you close the console at this point kittens will die");
     StartDaGame();
@@ -166,7 +271,8 @@ void HandleIOShit()
 }
 
 void HandleFirstTime() // kinky
-{        
+{
+    if (!File.Exists(configpath)) CreateConfig();
     var file = File.CreateText(importantfileithinkpath);
     Console.WriteLine("Seeking game path...");
 
@@ -222,28 +328,56 @@ void HandleCustomPath()
     return;
 }
 
-void DeclarePaths(string rootpath)
+void CreateConfig()
 {
-    gamePath = rootpath;
-    paks = $@"{rootpath.Replace("\n", "").Replace("\r", "")}\modcontent\PAKs\";
-    modvo = $@"{rootpath.Replace("\n", "").Replace("\r", "")}\modcontent\VO\";
-    modbanks = $@"{rootpath.Replace("\n", "").Replace("\r", "")}\modcontent\FMOD\";
-    gamevotemp = $@"{rootpath.Replace("\n", "").Replace("\r", "")}\modcontent\VO\GameTemp";
-    gamebanktemp = $@"{rootpath.Replace("\n", "").Replace("\r", "")}\modcontent\FMOD\GameTemp";
-    vo = $@"{rootpath.Replace("\n", "").Replace("\r", "")}\ReadyOrNot\Content\VO";
-    banks = $@"{rootpath.Replace("\n", "").Replace("\r", "")}\ReadyOrNot\Content\FMOD\Desktop";
-    gamePaks = $@"{rootpath.Replace("\n", "").Replace("\r", "")}\ReadyOrNot\Content\Paks";
+    XElement ops = new XElement("config",
+    new XElement("overrideall", false),
+    new XElement("skipvo", false),
+    new XElement("skipfmod", false),
+    new XElement("skippak", false)
+    );
+    ops.Save(configpath);
 }
 
-void ThrowException(Exception e, string path, string method)
+void DeclarePaths(string rootpath)
+{
+    gamePath = rootpath.Replace("\n", "").Replace("\r", "");
+    paks = $@"{gamePath}\modcontent\PAKs\";
+    modvo = $@"{gamePath}\modcontent\VO\";
+    modbanks = $@"{gamePath}\modcontent\FMOD\";
+    gamevotemp = $@"{gamePath}\modcontent\VO\GameTemp";
+    gamebanktemp = $@"{gamePath}\modcontent\FMOD\GameTemp";
+    vo = $@"{gamePath}\ReadyOrNot\Content\VO";
+    banks = $@"{gamePath}\ReadyOrNot\Content\FMOD\Desktop";
+    gamePaks = $@"{gamePath}\ReadyOrNot\Content\Paks";
+}
+
+void ThrowException(Exception e, string path, string method, string message)
 {
     Console.WriteLine(Environment.NewLine);
     Console.WriteLine("Attempted I/O Process Failed!");
+    Console.WriteLine(message);
     Console.WriteLine($"TRACE: {method} => {path}");
     Console.WriteLine($"!!EXCEPTION THROWN!! => {e}");
     Console.WriteLine(Environment.NewLine);
-    Console.WriteLine("It is recommended to purge all launcher files so that new ones can be generated");
+    Console.WriteLine("It is recommended to purge all launcher files (importanttext / modcontent) so that new ones can be generated");
     System.Timers.Timer timer = new System.Timers.Timer(5000);
     timer.Start();
     timer.Elapsed += (sender, e) => { Environment.Exit(1); };
+}
+
+bool[] GetConfigValues()
+{
+    XmlDocument file = new XmlDocument();
+    List<bool> confvals = new();
+    file.Load(configpath);
+    XmlNodeList node = file.GetElementsByTagName("overrideall");
+    confvals.Add(bool.Parse(node[0].InnerText));
+    node = file.GetElementsByTagName("skipvo");
+    confvals.Add(bool.Parse(node[0].InnerText));
+    node = file.GetElementsByTagName("skipfmod");
+    confvals.Add(bool.Parse(node[0].InnerText));
+    node = file.GetElementsByTagName("skippak");
+    confvals.Add(bool.Parse(node[0].InnerText));
+    return confvals.ToArray();
 }
